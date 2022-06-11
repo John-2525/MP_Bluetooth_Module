218c44253b4f5e6111049ff83bac5b4ae939b3c5
DAI KOI YIM 2001062E <2001062E@student.tp.edu.sg>
2022-06-12 02:26:58 +0800

    Can record audio, store it in custom folder and play it
    Unimplemented : Reminder function, image album and games, backend and posting of image album for images, videos and mp3, front end for displaying
    
    TODO : Finish documenting the remaining 2 functions

--- app/src/main/java/com/example/mp_bluetooth_module/Upload_File_Firebase.java
+++ app/src/main/java/com/example/mp_bluetooth_module/Upload_File_Firebase.java
@@ -12,6 +12,7 @@ import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -40,11 +41,12 @@ public class Upload_File_Firebase extends AppCompatActivity {
     private CountDownTimer InterruptTimer;
     private Button UploadBtn, BackBtn, RecordBtn, StopRecordingBtn,PlayAudioBtn;
     private MediaRecorder mRecorder;
+    private MediaPlayer mediaPlayer;
     private EditText FilesName;
     private Context context;
     private StorageReference mstorageReference;
     private String mFileName, FolderPath;
-    private File file, AudioFile;
+    private File AudioFolder, AudioFile, AudioPath;
 
 
     @Override
@@ -65,106 +67,34 @@ public class Upload_File_Firebase extends AppCompatActivity {
         CreateFolderAndGetExternalPermission();
         getMicrophonePermission();
 
-    }
-
-    private void askExtStoragePermission() {
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUEST_CODE);
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if(requestCode == PERMISSION_REQUEST_CODE) {
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                createDirectory(FolderPath);
+        /** Listens to any clicks on record button */
+        RecordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /** Function to start audio recording from phone mic */
+                startRecording();
             }
-        }
-        else {
-            Toast.makeText(Upload_File_Firebase.this,"Permission Denied",Toast.LENGTH_SHORT).show();
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
+        });
 
-
-    private void createDirectory(String Folder_Name) {
-        file = new File(Environment.getExternalStorageDirectory(), Folder_Name);
-        Log.d(TAG,Environment.getDataDirectory().toString());
-        /** Checks if the folder already exists in the external storage of the phone */
-        if(!file.exists()) {
-            /** If it does not exist then it creates it */
-            file.mkdirs();
-            Toast.makeText(Upload_File_Firebase.this, "Folder Made", Toast.LENGTH_SHORT).show();
-        }
-    //    else {
-    //        Toast.makeText(Upload_File_Firebase.this, "Folder is already made", Toast.LENGTH_SHORT).show();
-    //    }
-    }
-
-
-    private void CreateFolderAndGetExternalPermission() {
-        /** Checks if permission to write to external storage is granted */
-        if(ContextCompat.checkSelfPermission(Upload_File_Firebase.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            createDirectory(FolderPath);
-        }
-
-        else {
-            /** Gets permisiion to do so if it is not granted */
-            askExtStoragePermission();
-        }
-    }
-
-
-    private void getMicrophonePermission() {
-        if(this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE)) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSION_REQUEST_CODE);
+        /** Listens to any clicks on stop recording button */
+        StopRecordingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /** Function to stop audio recording from phone mic */
+                stopRecording();
             }
-        }
-    }
+        });
 
-
-
-    /** Function to start audio recording using device's built in microphone */
-    private void startRecording() {
-        /** Checks if the EditText is null to prevent null name or error when .setOutputFile */
-        mFileName = getFileName();
-        if(mFileName != null) {
-            Log.d(TAG,"File name is "+mFileName);
-            mRecorder = new MediaRecorder();
-            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            mRecorder.setOutputFile();
-
-            try {
-                mRecorder.prepare();
+        /** Listens to any clicks on play audio button */
+        PlayAudioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /** Function to play recorded audio from storage */
+                PlayAudio();
             }
-            catch(Exception e) {
-                Log.e(TAG,"Recorder prepare() failed");
-            }
-
-            mRecorder.start();
-        }
-        else {
-            Toast.makeText(context,"Please key in a file name",Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    /** Function to stop audio recording from device's built in microphone */
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
-
-        //uploadAudio();
-    }
-
-    private void uploadAudio() {
-        //StorageReference filepath = mstorageReference
+        });
 
     }
-
 
     /** Function to get String text from EditText in Upload_File_Firebase layout */
     public String getFileName() {
@@ -177,13 +107,222 @@ public class Upload_File_Firebase extends AppCompatActivity {
         }
     }
 
-    private String getAudioRecordingFilePath() {
-        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
-        AudioFile = contextWrapper.getExternalFilesDir(Environment.);
-
+    /** Function to ask user permission to write to external storage */
+    private void askExtStoragePermission() {
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUEST_CODE);
     }
 
 
+    /** Override method on what to do if the user gives/does not give permission to write to external storage */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        /** Checks if the code is for requesting permissions (?) */
+        if(requestCode == PERMISSION_REQUEST_CODE) {
+            /** Checks if the permission to write external storage is granted by user */
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                /** Creates a folder to house audio recordings */
+                createDirectory(FolderPath);
+            }
+        }
+        /** If users dont give permission to write to external storage */
+        else {
+            Toast.makeText(Upload_File_Firebase.this,"Permission Denied",Toast.LENGTH_SHORT).show();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    /** Function to check if permission to write external storage is given when coming to this activity */
+    private void CreateFolderAndGetExternalPermission() {
+        /** Checks if permission to write to external storage is granted */
+        if(ContextCompat.checkSelfPermission(Upload_File_Firebase.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            createDirectory(FolderPath);
+        }
+
+        else {
+            /** Gets permission to do so if it is not granted */
+            askExtStoragePermission();
+        }
+    }
+
+
+    /** Function to create a folder within the com.example.~~ folder within the phone storage, located at Android/data/~~*/
+    private void createDirectory(String Folder_Name) {
+
+        /** ContextWrapper == Proxying implementation of Context that simply delegates all of its calls to another
+         *  Context. Can be subclassed to modify behavior without changing the original Context.
+         *
+         *  getApplicationContext == Return the context of the single, global Application object of the current process.
+         *  This generally should only be used if you need a Context whose lifecycle is separate from the current context, that
+         *  is tied to the lifetime of the process rather than the current component.
+         *
+         *  From https://developer.android.com/reference/android/content/ContextWrapper
+         *  TODO Need to understand and rewrite this part
+         */
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        /**
+         * According to https://stackoverflow.com/questions/23049100/getexternalfilesdir-not-creating-new-directory,
+         * when calling .getExternalFilesDir(), the custom directory will automatically be created, works fine as
+         * folder to store Audio recordings is recorded when switching to this activity
+         *
+         * Documentation on it ( https://developer.android.com/reference/android/content/ContextWrapper#getExternalFilesDirs(java.lang.String) )
+         * doesnt say anything related to that, forum reply from May 2021 claims that the online documentation needs to be updated regarding this
+         */
+        AudioFolder = contextWrapper.getExternalFilesDir(Folder_Name);
+        /** Checks if the folder has been made or already exists */
+        if(!AudioFolder.exists()) {
+            Toast.makeText(Upload_File_Firebase.this, "Error folder not made", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(Upload_File_Firebase.this, "Folder Made/Exists", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    private void getMicrophonePermission() {
+    /** Check whether the given feature name is one of the available features as returned by getSystemAvailableFeatures() */
+        if(this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE)) {
+            /** Checks if permission to use the microphone is denied or not granted yet */
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_DENIED) {
+                /** Requests microphone permission to be granted to this application */
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    /** Function to start audio recording using device's built in microphone */
+    private void startRecording() {
+        /** Gets the file name from the EditText and set it to a variable */
+        mFileName = getFileName();
+        /** Checks if the EditText is empty to prevent null name or error when .setOutputFile */
+        if(!mFileName.isEmpty()) {
+            Log.d(TAG,"File name is "+mFileName);
+            /**
+             * When writing the codes below, there is a certain order to them, which is stated in
+             * the documentation due to the MediaRecorder state diagram in
+             * https://developer.android.com/reference/android/media/MediaRecorder
+             */
+
+            /** Creates a new instance of MediaRecorder */
+            mRecorder = new MediaRecorder();
+            /**
+             * Sets the audio source for recording, The source needs to be specified before setting
+             * recording-parameters or encoders. Call this only before setOutputFormat()
+             */
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            /**
+             * Sets the format of the output file produced during recording.
+             * Call this after setAudioSource()/setVideoSource() but before prepare().
+             *
+             * It is recommended to always use 3GP format when using the H.263 video encoder and AMR audio encoder.
+             * Using an MPEG-4 container format may confuse some desktop players
+             */
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            /** Pass in the file object to be written. Call this after setOutputFormat() but before prepare() */
+            mRecorder.setOutputFile(getAudioRecordingFilePath(FolderPath));
+            /**
+             * Sets the audio encoder to be used for recording. If this method is not called,
+             * the output file will not contain an audio track.
+             * Call this after setOutputFormat() but before prepare().
+             */
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            try {
+                /**
+                 * Prepares the recorder to begin capturing and encoding data.
+                 * This method must be called after setting up the desired audio and video sources,
+                 * encoders, file format, etc., but before start().
+                 */
+                mRecorder.prepare();
+            }
+            catch(Exception e) {
+                Log.e(TAG,"Recorder prepare() failed");
+                e.printStackTrace();
+            }
+
+            try {
+                /**
+                 * Begins capturing and encoding data to the file specified with setOutputFile().
+                 * Call this after prepare().
+                 */
+                mRecorder.start();
+            }
+            catch (Exception e) {
+                Log.e(TAG,"Recorder start() failed");
+                e.printStackTrace();
+            }
+            Toast.makeText(Upload_File_Firebase.this, "Recording Started", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(Upload_File_Firebase.this,"Please key in a file name",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    /** Function to stop audio recording from device's built in microphone */
+    private void stopRecording() {
+        /**
+         * Stops recording. Call this after start().
+         * Once recording is stopped, you will have to configure it again as if it has just been constructed.
+         *
+         * Note that a RuntimeException is intentionally thrown to the application, if no valid audio/video data has
+         * been received when stop() is called.
+         *
+         * This can happen for instance if stop() is called immediately after start().
+         * The failure lets the application take action accordingly to clean up the output file like
+         * for instance, delete th output file. As the output file will not be properly constructed when this happens.
+         */
+        mRecorder.stop();
+        /**
+         * Releases resources associated with this MediaRecorder object.
+         * It is good practice to call this method when you're done using the MediaRecorder
+         *
+         * If not called, unnecessary resources like memory and codec will be held, increase
+         * battery consumption and recording failure for other applications due to running multiple
+         * instances of the same codec being unsupported
+         * or degraded performance if multiple codec instances are supported
+         */
+        mRecorder.release();
+        mRecorder = null;
+        Toast.makeText(Upload_File_Firebase.this, "Recording Stopped", Toast.LENGTH_SHORT).show();
+
+        //uploadAudio();
+    }
+
+    private void PlayAudio() {
+        try {
+            /** Create new instance of media player */
+            mediaPlayer = new MediaPlayer();
+
+            mediaPlayer.setDataSource(getAudioRecordingFilePath(FolderPath));
+
+            mediaPlayer.prepare();
+
+            mediaPlayer.start();
+            Toast.makeText(Upload_File_Firebase.this, "Playing Recording", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e) {
+            Log.e(TAG,"Audio failed to play");
+            e.printStackTrace();
+        }
+    }
+
+    private String getAudioRecordingFilePath(String FolderPath) {
+        /** See createDirectory(String Folder_Name) for the explanation of the 2 lines below this documentation */
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        AudioPath = contextWrapper.getExternalFilesDir(FolderPath);
+
+        AudioFile = new File(AudioPath,getFileName()+".3gp");
+        return AudioFile.getPath();
+    }
+
+
+    private void uploadAudio() {
+        //StorageReference filepath = mstorageReference
+
+    }
 
 
     /** Defines callbacks for service binding, passed to bindService() */
@@ -208,7 +347,7 @@ public class Upload_File_Firebase extends AppCompatActivity {
     /**
      * Its important to unbind activities or else some errors will occur (?) due to
      * multiple services running together
-     * ( Confirm this theory )
+     * ( TODO Confirm this theory )
      */
 
 
